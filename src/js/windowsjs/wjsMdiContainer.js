af0b61e9402fecfe1b9f5eecbab124bb96dabf74
Javier Lasterra Lalaguna <jlasterra@MacBook-Pro-de-Javier.local>
2018-10-03 16:27:26 +0200

    mdiContainer is not dragable  and not sizable

--- src/js/windowsjs/wjsMdiContainer.js
+++ src/js/windowsjs/wjsMdiContainer.js
@@ -27,20 +27,18 @@ function WsjMdiContainer (svgMain) {
 
     function initialize () {
 
-        //var svgMain = create();
         create();
         _menu = Menu(svgMain, _color);
         _menu.initialize();
 
-        makeDraggable();
-        makeSizable();
+        //makeDraggable();
+        //makeSizable();
     }
 
     function makeDraggable() {
     
         var offset;
         var selectedElement;
-        //var svgMain =  document.getElementById('svgMain');
     
         svgMain.addEventListener('mousedown', startDrag);
         svgMain.addEventListener('mousemove', drag);
@@ -86,7 +84,6 @@ function WsjMdiContainer (svgMain) {
 
         var offset = {};
         var selectedElement;
-        //var svgMain =  document.getElementById('svgMain');
     
         svgMain.addEventListener('mousedown', startDrag);
         svgMain.addEventListener('mousemove', drag);
@@ -139,18 +136,10 @@ function WsjMdiContainer (svgMain) {
     }
 
     function create () {
-
-        // var svgMain = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        // svgMain.setAttribute('width',window.innerWidth);
-        // svgMain.setAttribute('height',window.innerHeight);
-        // svgMain.setAttribute('id','svgMain');
     
         var rec1 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         rec1.setAttributeNS(null, 'id', 'mdi_rec1');
         svgMain.appendChild(rec1);
-
-        console.log('hola');
-        console.log(svgMain);
     
         var line1 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
         line1.setAttributeNS(null, 'id', 'mdi_line1');
@@ -160,23 +149,23 @@ function WsjMdiContainer (svgMain) {
         line2.setAttributeNS(null, 'id', 'mdi_line2');
         svgMain.appendChild(line2);
     
-        var rec2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-        rec2.setAttributeNS(null, 'id', 'mdi_rec2');
-        rec2.setAttributeNS(null, 'class', 'draggable');
-        svgMain.appendChild(rec2);
+        // var rec2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        // rec2.setAttributeNS(null, 'id', 'mdi_rec2');
+        // //rec2.setAttributeNS(null, 'class', 'draggable');
+        // svgMain.appendChild(rec2);
     
-        var line3 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        line3.setAttributeNS(null, 'id', 'mdi_line3');
-        svgMain.appendChild(line3);
+        // var line3 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        // line3.setAttributeNS(null, 'id', 'mdi_line3');
+        // svgMain.appendChild(line3);
     
-        var line4 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        line4.setAttributeNS(null, 'id', 'mdi_line4');
-        svgMain.appendChild(line4);
+        // var line4 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        // line4.setAttributeNS(null, 'id', 'mdi_line4');
+        // svgMain.appendChild(line4);
     
-        var tri1 = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
-        tri1.setAttributeNS(null, 'id', 'mdi_tri1');
-        tri1.setAttributeNS(null, 'class', 'sizable');
-        svgMain.appendChild(tri1);
+        // var tri1 = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+        // tri1.setAttributeNS(null, 'id', 'mdi_tri1');
+        // tri1.setAttributeNS(null, 'class', 'sizable');
+        // svgMain.appendChild(tri1);
     
         document.body.appendChild(svgMain);
 
@@ -202,13 +191,13 @@ function WsjMdiContainer (svgMain) {
         rec1.setAttributeNS(null, 'stroke', _color);
         rec1.setAttributeNS(null, 'stroke-width', '1'); 
     
-        var rec2 = document.getElementById('mdi_rec2');
-        rec2.setAttributeNS(null, 'x', _startX);
-        rec2.setAttributeNS(null, 'y', _startY);
-        rec2.setAttributeNS(null, 'width', _width);
-        rec2.setAttributeNS(null, 'height', _offsetLine2);
-        rec2.setAttributeNS(null, 'stroke', _color);
-        rec2.setAttributeNS(null, 'stroke-width', '1'); 
+        // var rec2 = document.getElementById('mdi_rec2');
+        // rec2.setAttributeNS(null, 'x', _startX);
+        // rec2.setAttributeNS(null, 'y', _startY);
+        // rec2.setAttributeNS(null, 'width', _width);
+        // rec2.setAttributeNS(null, 'height', _offsetLine2);
+        // rec2.setAttributeNS(null, 'stroke', _color);
+        // rec2.setAttributeNS(null, 'stroke-width', '1'); 
     
         var line1 = document.getElementById('mdi_line1');
         line1.setAttributeNS(null, 'x1', _startX);
@@ -226,36 +215,36 @@ function WsjMdiContainer (svgMain) {
         line2.setAttributeNS(null, 'stroke', _color);
         line2.setAttributeNS(null, 'stroke-width', '1');
     
-        var tri1 = document.getElementById('mdi_tri1');
-        var tri1Points = [ [_width + _startX, _height + _startY], 
-            [_width + _startX - ( offset3 * 3 ) , _height + _startY],
-            [_width + _startX, _height + _startY - ( offset3 * 3 )] 
-        ];
-        var res = [];
-        for (var i = 0, l = tri1Points.length; i < l; i++) {
-            res.push(tri1Points[i].join(','));
-        }
-        var tri1PointsString = res.join(' '); 
-        tri1.setAttributeNS(null, 'points', tri1PointsString);
-        tri1.setAttributeNS(null, 'stroke', _color);
-        tri1.setAttributeNS(null, 'stroke-width', '1'); 
-        tri1.setAttributeNS(null, 'fill-opacity', '0'); 
+        // var tri1 = document.getElementById('mdi_tri1');
+        // var tri1Points = [ [_width + _startX, _height + _startY], 
+        //     [_width + _startX - ( offset3 * 3 ) , _height + _startY],
+        //     [_width + _startX, _height + _startY - ( offset3 * 3 )] 
+        // ];
+        // var res = [];
+        // for (var i = 0, l = tri1Points.length; i < l; i++) {
+        //     res.push(tri1Points[i].join(','));
+        // }
+        // var tri1PointsString = res.join(' '); 
+        // tri1.setAttributeNS(null, 'points', tri1PointsString);
+        // tri1.setAttributeNS(null, 'stroke', _color);
+        // tri1.setAttributeNS(null, 'stroke-width', '1'); 
+        // tri1.setAttributeNS(null, 'fill-opacity', '0'); 
     
-        var line3 = document.getElementById('mdi_line3');
-        line3.setAttributeNS(null, 'x1', _width + _startX - ( offset3 * 2));
-        line3.setAttributeNS(null, 'y1', _height + _startY);
-        line3.setAttributeNS(null, 'x2', _width + _startX);
-        line3.setAttributeNS(null, 'y2', _height + _startY - (offset3 * 2));
-        line3.setAttributeNS(null, 'stroke', _color);
-        line3.setAttributeNS(null, 'stroke-width', '1');
+        // var line3 = document.getElementById('mdi_line3');
+        // line3.setAttributeNS(null, 'x1', _width + _startX - ( offset3 * 2));
+        // line3.setAttributeNS(null, 'y1', _height + _startY);
+        // line3.setAttributeNS(null, 'x2', _width + _startX);
+        // line3.setAttributeNS(null, 'y2', _height + _startY - (offset3 * 2));
+        // line3.setAttributeNS(null, 'stroke', _color);
+        // line3.setAttributeNS(null, 'stroke-width', '1');
     
-        var line4 = document.getElementById('mdi_line4');
-        line4.setAttributeNS(null, 'x1', _width + _startX - ( offset3));
-        line4.setAttributeNS(null, 'y1', _height + _startY);
-        line4.setAttributeNS(null, 'x2', _width + _startX);
-        line4.setAttributeNS(null, 'y2', _height + _startY - (offset3));
-        line4.setAttributeNS(null, 'stroke', _color);
-        line4.setAttributeNS(null, 'stroke-width', '1');
+        // var line4 = document.getElementById('mdi_line4');
+        // line4.setAttributeNS(null, 'x1', _width + _startX - ( offset3));
+        // line4.setAttributeNS(null, 'y1', _height + _startY);
+        // line4.setAttributeNS(null, 'x2', _width + _startX);
+        // line4.setAttributeNS(null, 'y2', _height + _startY - (offset3));
+        // line4.setAttributeNS(null, 'stroke', _color);
+        // line4.setAttributeNS(null, 'stroke-width', '1');
 
         _menu.draw();
     }
